979d84bbaa182887581d6facfbbfe63524dcacd1
Carlos Eduardo Lores Hernández <carloseduardoloreshernandez@MacBook-Pro-de-Carlos-2.local>
2019-09-20 21:45:01 +0200

    detalles de heroes creados y primeros pipes

--- src/app/components/shared/heroe/heroe.component.ts
+++ src/app/components/shared/heroe/heroe.component.ts
@@ -16,6 +16,7 @@ export class HeroeComponent{
             ){ 
     this.activateRoute.params.subscribe(params=>{
       this.heroe = this._heroesService.getHeroe(params['id']);
+      console.log(this.heroe)
     })
   }
 
